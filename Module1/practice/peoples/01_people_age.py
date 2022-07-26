5705ca987433f88f99f9b5b7faf8a48a0eb5c661
utenkov <utenkov@list.ru>
2022-07-26 09:14:40 +0300

    people_01_v2

--- Module1/practice/peoples/01_people_age.py
+++ Module1/practice/peoples/01_people_age.py
@@ -8,12 +8,10 @@ class People:
         # Корректным возрастом считаем целое число от 1 до 100
         # Если задан некорректный возраст, выводим "некорректное значение для возраста" и оставляем старое значение
         # Метод ничего не возвращает в качестве результата, а только меняем свойство self.age на значение new_age
-        # TODO: напишите реализацию данного метода
-
         if type(new_age) == int and 1 <= new_age <= 100:
             self.__age = new_age
         else:
-            print("некорректное значение для возраста")
+            raise ValueError("Некорректное значение для возраста")
 
     def full_name(self):
         return f"{self.surname} {self.name}"
@@ -32,10 +30,18 @@ print(people2.full_info())
 print(people3.full_info())
 
 print("Меняем возраст людей")
-people1.change_age(45)
-people2.change_age("help")
-people2.change_age(-30)
-
+try:
+    people1.change_age(45)
+except ValueError as ex:
+    print(ex)
+try:
+    people2.change_age("help")
+except ValueError as ex:
+    print(ex)
+try:
+    people3.change_age(25)
+except ValueError as ex:
+    print(ex)
 print(people1.full_info())
 print(people2.full_info())
 print(people3.full_info())
